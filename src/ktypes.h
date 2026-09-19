debe2fd49a8cb26752078ef17d80f2e250b2ab79
AtesIsf <isfendiyar510@gmail.com>
2026-09-19 00:36:09 -0400

    fixed limits

--- src/ktypes.h
+++ src/ktypes.h
@@ -27,10 +27,10 @@ typedef uint8 byte;
 #define INT32_MAX (__INT32_MAX__)
 #define INT64_MAX (__INT64_MAX__)
 
-#define INT8_MIN  (INT8_MAX + 1)
-#define INT16_MIN (INT16_MAX + 1)
-#define INT32_MIN (INT32_MAX + 1)
-#define INT64_MIN (INT64_MAX + 1)
+#define INT8_MIN  (-INT8_MAX - 1)
+#define INT16_MIN (-INT16_MAX - 1)
+#define INT32_MIN (-INT32_MAX - 1)
+#define INT64_MIN (-INT64_MAX - 1)
 
 #define UINT8_MAX  (__UINT8_MAX__)
 #define UINT16_MAX (__UINT16_MAX__)
