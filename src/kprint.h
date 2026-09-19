5f7b69bc176b7aa45de5d82c3414b98302b8cbb1
AtesIsf <isfendiyar510@gmail.com>
2026-09-19 00:38:34 -0400

    updated header file to match the source code

--- src/kprint.h
+++ src/kprint.h
@@ -1,12 +1,19 @@
 #ifndef KPRINT_H
 #define KPRINT_H
 
+#include <stdarg.h>
 #include "ktypes.h"
 
 #define UART_ADDR ((unsigned char *) 0x10000000) 
 
 status_code kputc(char c);
 
+status_code kprint(const char *str);
+
 status_code kputs(const char *str);
+
+void kvprintf(const char *format, va_list arg);
+
+void kprintf(const char *format, ...);
 
 #endif
