c286d11fc5d87626241a608e7cc8c1752528ab6c
AtesIsf <isfendiyar510@gmail.com>
2026-09-19 00:03:36 -0400

    defined limits

--- src/ktypes.h
+++ src/ktypes.h
@@ -21,6 +21,22 @@ typedef __UINT64_TYPE__ uint64;
 typedef uint8 byte;
 #define NULL ((void *) 0x0)
 
+// type limits
+#define INT8_MAX  (__INT8_MAX__)
+#define INT16_MAX (__INT16_MAX__)
+#define INT32_MAX (__INT32_MAX__)
+#define INT64_MAX (__INT64_MAX__)
+
+#define INT8_MIN  (INT8_MAX + 1)
+#define INT16_MIN (INT16_MAX + 1)
+#define INT32_MIN (INT32_MAX + 1)
+#define INT64_MIN (INT64_MAX + 1)
+
+#define UINT8_MAX  (__UINT8_MAX__)
+#define UINT16_MAX (__UINT16_MAX__)
+#define UINT32_MAX (__UINT32_MAX__)
+#define UINT64_MAX (__UINT64_MAX__)
+
 // process exit codes constants
 typedef byte status_code;
 #define OK (0)
